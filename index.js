7f7abb8b35246444ea21d0308c09f0a125f99a29
Marco Llapapasca <marco.llapapasca@bluetab.net>
2019-12-03 15:26:12 -0500

    first commint

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// variables Globales
+const express = require('express');
+
+// Intializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+
+app.get('/', (req, res)=>{
+    res.send('iDeveloper') 
+})
+
+// Starting
+app.listen(app.get('port'), () => {
+    console.log(`Start server port: ${app.get('port')}`);
+});
